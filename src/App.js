85e3d57b5806fd38793f95b4390558cd44f8ffee
Anna CUILHE <anna.cuilhe-ext@socgen.com>
2020-01-15 00:58:54 +0100

    marche pas

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 import HelloWorld from "./HelloWorld";
 
 
-function Affichage(props) {
-return <h1>Toto, {props.azerty}</h1>
+
+
+function Affichage1(props) {
+return <h1>To do, {props.azerty}</h1>
 }
+
+function Affichage2(props) {
+  return <h1>Done, {props.ytreza}</h1>
+  }
 
 function Item(props) {
   return ( <div onClick={props.onClick}>
@@ -15,14 +21,46 @@ function Item(props) {
 </div>);
 }
 
+function List(props) {
+  const [items, setItems] = React.useState(init);
+
+  const done = (id,checked) => {
+    setItems(state => {
+       return state.map(item => {
+         console.log(item);
+         if (item.id !== id) return item;
+         return {...item, done: !checked};
+       });
+     });
+   }
+
+  return <div><h1>{props.title}</h1>
+          <div>
+            {items
+              .filter(item => item.done === props.checked)
+              .map(item => {
+                return (
+                  <Item key={item.id} label={item.label} onClick={() => done(item.id,props.checked)} checked={props.checked} />
+                ); 
+              })
+            }
+          </div>
+        </div>
+}
+
 const init = [
-  {id:1, label: 'todo1', done:false},
-  {id:2, label: 'todo2', done:false},
-  {id:3, label: 'todo3', done:false},
-  {id:4, label: 'todo4', done:true},
+  {id:1, label: 'java', done:false},
+  {id:2, label: 'javascript', done:false},
+  {id:3, label: 'react', done:false},
+  {id:4, label: 'node', done:true},
 ]
 
 function App() {
+  return <div>
+          <List title="ToDo, à faire" todos={init} checked={false}/>
+          <List title="Done, fait" todos={init} checked={true}/>
+        </div>
+  /*
   const [todos, setTodos] = React.useState(init);
  
   console.log("RENDER")
@@ -43,26 +81,28 @@ function App() {
     });
   });
   }
+  
   return <div>
-    <Affichage azerty="kikou" />
+    <Affichage1 azerty="à faire" />
     <hr/>
     {todos
       .filter(todo => todo.done === false)
       .map(todo => {
         return (
-          <Item label={todo.label} onClick={() => done(todo.id)} checked={true}/>
+          <Item label={todo.label} onClick={() => done(todo.id)} />
         ); 
       })
     }
     <hr/>
+    <Affichage2 ytreza="fait" />
     {todos
       .filter(todo => todo.done === true)
       .map(todo => {
-        return <Item label={todo.label} onClick={() => undone(todo.id)} />
+        return <Item label={todo.label} onClick={() => undone(todo.id)} checked={true}/>
       })
     }
   </div>
-
+*/
 }
 
 
